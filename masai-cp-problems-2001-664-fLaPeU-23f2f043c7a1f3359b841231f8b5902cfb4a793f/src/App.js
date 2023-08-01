c13e8092b8272a1f591f9ae3ed4af8a9b9f3eaaa
unknown <vedican.v44@gmail.com>
2023-08-01 18:31:46 +0530

    final

--- masai-cp-problems-2001-664-fLaPeU-23f2f043c7a1f3359b841231f8b5902cfb4a793f/src/App.js
+++ masai-cp-problems-2001-664-fLaPeU-23f2f043c7a1f3359b841231f8b5902cfb4a793f/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import { AllRoutes } from "./Routes/AllRoutes";
+import { Navbar } from "./Routes/Navbar";
 
 export default function App() {
   return (
     <div className="App">
-
+             <h1>haloo mandeep</h1>
+             <Navbar></Navbar>
+             <AllRoutes></AllRoutes>
     </div>
   );
 }
--- masai-cp-problems-2001-664-fLaPeU-23f2f043c7a1f3359b841231f8b5902cfb4a793f/src/Pages/Login.jsx
+++ masai-cp-problems-2001-664-fLaPeU-23f2f043c7a1f3359b841231f8b5902cfb4a793f/src/Pages/Login.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { Authcontext } from '../Context/AuthContextProvider';
+import { Navigate } from 'react-router-dom';
 
 export default function Login() {
-    
+    const {isAuth, login} = useContext(Authcontext);
+    const [user, setUser]= useState({
+        email: "",
+        password:""
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch("https://reqres.in/api/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(user)
+        })
+            .then((res) => res.json())
+            .then((res) => user.email === "eve.holt@reqres.in" && user.password === "cityslicka" && login(res))
+    }
+
+    if(isAuth){
+     return   <Navigate to="/"/>
+    }
+
     return (
         <div>
-            <form className = "auth_form"  >
+            <form className = "auth_form" onSubmit={(e)=> handleSubmit(e)} >
                 <input
                     style = {{padding:"5px", margin: "10px", width: 200}}
                     type = "email"
                     className = "email"
                     placeholder = "Enter Email"
+                    onChange={(e)=> setUser({...user, email: e.target.value})}
                 />
                 <br />
                 <input
@@ -17,6 +41,7 @@ export default function Login() {
                     type = "password"
                     className = "password"
                     placeholder = "Enter password"
+                    onChange={(e)=> setUser({...user, password: e.target.value})}
                 />
                 <br />
                 <input className = "submit" type = "submit"/>
